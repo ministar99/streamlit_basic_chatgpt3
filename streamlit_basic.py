e244e9ecd29aeca976e1b7944edb47c7b3bb6e4b
kimmingyu <princemin9947@gmail.com>
2025-12-16 11:28:37 +0900

    second commit

--- streamlit_basic.py
+++ streamlit_basic.py
@@ -17,7 +17,7 @@ st.title("김민규")
 
 # (1) st.session_state에 "messages"가 없으면 초기값을 설정
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "무엇을 도와드릴까요?"}]
 
 # (2) 대화 기록을 출력
 for msg in st.session_state.messages:
